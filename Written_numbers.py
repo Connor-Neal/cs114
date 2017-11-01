9faa8e72adf67aeb37f3b8ec01706831837cdd10
s203 <s203@AI241LAB0203M16.local>
2017-11-01 11:36:04 -0700

    updates

---
+++ Written_numbers.py
@@ -0,0 +1,73 @@
+print('hio')
+
+num = int(input())
+
+tens = nums // 10
+one = num % 10
+
+def getAnswer(answerNumber):
+if tens = 1:
+    return = 'ten'
+
+    elif tens = 2:
+        return = 'twenty'
+
+    elif tens = 3:
+        return = 'threety'
+
+    elif tens = 4:
+    return = 'fourty'
+
+    elif tens = 5:
+    return = 'fifty'
+
+    elif tens = 6:
+    return = 'sixty'
+
+    elif tens = 7:
+    return = 'seventy'
+
+    elif tens = 8:
+    return = 'eightty'
+
+    elif tens = 9:
+    return = 'ninety'
+
+ten = getAnswer(tens)
+
+def getAnswer(answerNumber):
+if ones = 1:
+    return = 'one'
+
+    elif ones = 2:
+        return = 'two'
+
+    elif ones = 3:
+        return = 'three'
+
+    elif ones = 4:
+        return = 'four'
+
+    elif ones = 5:
+        return = 'five'
+
+    elif ones = 6:
+        return = 'six'
+
+    elif ones = 7:
+        return = 'seven'
+
+    elif ones = 8:
+        return = 'eight'
+
+    elif ones = 9:
+        return = 'nine'
+
+    elif ones = 0:
+        return = 'Zero'
+
+one = getAnswer(ones)
+if num < 10:
+    print(one)
+else:
+    print(ten,one)
