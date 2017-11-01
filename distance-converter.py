5f86f32006440a89b047631b65795080c235a8d1
s203 <s203@AI241LAB0203M16.local>
2017-11-01 12:00:49 -0700

    updates

---
+++ distance-converter.py
@@ -0,0 +1,15 @@
+print('')
+print('Hello')
+print('')
+print('Give me unit')
+print('')
+unit1 = int(input())
+print('Give me distance')
+num = int(input())
+
+if unit1 = 'miles' or unit1 == 'mi' and unit2 = 'kilometers' or unit2 == 'km':
+    dist=(num = )
+
+print('')
+print('GOOOOOOOOD BYYYYYYYYE')
+print('')
